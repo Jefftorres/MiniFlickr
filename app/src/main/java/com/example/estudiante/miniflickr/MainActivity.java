ed5d2a26e684102850df106a37045e9a168ae3bb
ESTUDIANTE <estudiante@gmail.com>
2015-12-01 12:06:30 -0500

    Agregado botón de login con facebook.

--- app/src/main/java/com/example/estudiante/miniflickr/MainActivity.java
+++ app/src/main/java/com/example/estudiante/miniflickr/MainActivity.java
@@ -2,12 +2,47 @@ package com.example.estudiante.miniflickr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
+    private TextView header;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
+
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        header = (TextView) findViewById(R.id.txtLogin);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                header.setText(loginResult.getAccessToken().getUserId());
+            }
+
+            @Override
+            public void onCancel() {
+                header.setText("" + R.string.loginCancel);
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                header.setText("" + R.string.loginError);
+            }
+        });
     }
 }
